de5d0acc08564a50d3c0df88f43abe93e358cbf5
valian7 <58330049+valian7@users.noreply.github.com>
2019-12-16 05:45:08 +0700

    Add files via upload

---
+++ data/StrukturRuang_2.js
@@ -0,0 +1,10 @@
+var json_StrukturRuang_2 = {
+"type": "FeatureCollection",
+"name": "StrukturRuang_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Fungsi": "Pusat Pelayanan", "Desa": "Teloyo" }, "geometry": { "type": "Point", "coordinates": [ 110.760044448961807, -7.624848458419608 ] } },
+{ "type": "Feature", "properties": { "id": "2", "Fungsi": "Sub Pusat Pelayanan", "Desa": "Kingkang" }, "geometry": { "type": "Point", "coordinates": [ 110.755573159363948, -7.638753693902486 ] } },
+{ "type": "Feature", "properties": { "id": "3", "Fungsi": "Sub Pusat Pelayanan", "Desa": "Gunting" }, "geometry": { "type": "Point", "coordinates": [ 110.771500368320389, -7.646641183792943 ] } }
+]
+}
